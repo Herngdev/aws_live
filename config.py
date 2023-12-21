eb4d2a20396ca755ac3be1915d3fdaa28e2c6e60
Herngdev <96715462+Herngdev@users.noreply.github.com>
2023-12-22 01:19:45 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.c1ondx59uohl.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait_3213"
+customdb = "employee"
+custombucket = "cherngherng-employee"
+customregion = "us-east-1"
+
